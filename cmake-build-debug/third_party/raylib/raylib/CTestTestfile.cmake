baecab5d591d09f38dcb6f3870a97f4511468e9e
elisfromkirov <mozgg2002@gmail.com>
2023-05-22 14:49:53 +0300

    Fixes

--- cmake-build-debug/third_party/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nikitamoskalev/mipt/multiplayer/MIPT-networked/third_party/raylib/src
-# Build directory: /Users/nikitamoskalev/mipt/multiplayer/MIPT-networked/cmake-build-debug/third_party/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
